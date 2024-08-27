045207d9334f3802e2ed75a8e1163bc44737d0ec
TheoWebDev <theo.demarle@live.fr>
2024-08-27 10:13:27 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+export default function App() {
+  const [image, setImage] = useState(null)
+
+  const uploadImage = async (e) => {
+    const formData = new FormData()
+    formData.append('file', e.target.files[0])
+    setImage(e.target.files[0])
+    try {
+      const options = {
+        method: 'POST',
+        body: formData
+      }
+      const response = await fetch('http://localhost:8000/upload', options)
+      const data = await response.json()
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {image && <img src={URL.createObjectURL(image)} />}
+      <label htmlFor="files">Upload an image</label>
+      <input onChange={uploadImage} id="files" type="file" accept="image/*" hidden />
     </div>
   );
 }
-
-export default App;
